ec53d497a84c4956a6b21739288cbf88953c02ec
Pociatlo <k.podsadlo@gmail.com>
2023-04-07 22:08:02 +0200

    Section Done

--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -1,7 +1,9 @@
-import ChatBubble from "@/components/atoms/Chat-Bubble/Chat-Bubble";
+import Section from "@/components/atoms/Section/Section";
 
 export default function GamePage () {
     return (
-        <ChatBubble variant={1} owner={'Krzysztof'} date={new Date("October 13, 2014 11:13:00")} text={'NO elo elo co tam słychać'}/>
+        <Section title={true} titleText={'Next'} side={'left'} >
+            No elo
+        </Section>
     )
 }
