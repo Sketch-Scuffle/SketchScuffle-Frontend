97751420a87074d96333688ce911450cae648584
TacticalCrab <pkaleksandrowicz@gmail.com>
2023-04-08 18:42:31 +0200

    wip

--- components/atoms/Canvas/Canvas.tsx
+++ components/atoms/Canvas/Canvas.tsx
@@ -1,23 +1,49 @@
 import {MouseEventHandler, MouseEvent, useEffect, useRef, useState} from 'react';
 
+export enum ToolType {
+    Brush,
+    Fill,
+    Eraser
+}
+
+
 type Point = {
     x: number,
     y: number
 }
 
-interface CordsSet {
+export interface CordsSet {
     start: Point,
     end: Point
 }
 
-export default function Canvas() {
+export interface CanvasProps {
+    width?: number;
+    height?: number;
+    cordsDraw: CordsSet | null;
+    canDraw?: boolean;
+    color?: string;
+    lineWidth?: number;
+    onDraw?: (cords: CordsSet) => void;
+    toolType?: ToolType;
+}
+
+
+function getPixel(pixelData: any, x: number, y: number) {
+    if (x < 0 || y < 0 || x >= pixelData.width || y >= pixelData.height) {
+        return -1;  // impossible color
+    } else {
+        return pixelData.data[y * pixelData.width + x];
+    }
+}
+
+export default function Canvas({width=0, height=0, cordsDraw, canDraw=false, color='#000000', lineWidth=1, onDraw, toolType=ToolType.Brush}: CanvasProps) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const contextRef = useRef<CanvasRenderingContext2D | null>(null);
-    const [cords, setCords] = useState<CordsSet[]>([]);
     const [isDrawing, setIsDrawing] = useState(false);
+    const [prevCords, setPrevCords] = useState<Point| null>(null);
 
     useEffect(() => {
-        console.log("Rerender");
         const canvas = canvasRef.current;
         if (!canvas) { return; }
 
@@ -26,21 +52,102 @@ export default function Canvas() {
 
         context.lineCap = 'round';
         context.lineJoin = 'round';
-        context.strokeStyle = 'black';
-        context.lineWidth = 5;
+        context.strokeStyle = color;
+        context.lineWidth = lineWidth;
         context.imageSmoothingEnabled = true;
         contextRef.current = context;
-
+        console.log(context)
     }, [])
 
-    const handleMouseDown = (e: MouseEvent<HTMLCanvasElement>) => {
-        const {offsetX, offsetY} = e.nativeEvent;
-        contextRef.current?.beginPath();
-        contextRef.current?.moveTo(offsetX, offsetY);
-        contextRef.current?.lineTo(offsetX, offsetY);
-        contextRef.current?.stroke();
+    useEffect(() => {
+        if (!contextRef.current) {
+            return;
+        }
 
-        setIsDrawing(true);
+        contextRef.current.strokeStyle = color;
+        contextRef.current.lineWidth = lineWidth;
+    }, [color, lineWidth])
+
+    useEffect(() => {
+        if (cordsDraw) { draw(cordsDraw); }
+    }, [cordsDraw])
+
+
+    const fill = (ctx: CanvasRenderingContext2D, point: Point) => {
+        const fillColor = parseInt('0xff' + color?.slice(1).match(/.{1,2}/g)!.reverse().join(""), 16);
+        console.log(color);
+        console.log('0xff' + color?.slice(1).match(/.{1,2}/g)!.reverse().join(""))
+
+        const imageData = ctx.getImageData(0, 0, width, height)!;
+        const pixelData = {
+            width: imageData.width,
+            height: imageData.height,
+            data: new Uint32Array(imageData.data.buffer)
+        }
+
+        console.log(imageData);
+
+        const targetColor = getPixel(pixelData, point.x, point.y);
+        if (targetColor !== fillColor) {
+            const pixelsToCheck: number[] = [point.x, point.y];
+            while (pixelsToCheck.length > 0) {
+                const y = pixelsToCheck.pop()!;
+                const x = pixelsToCheck.pop()!;
+                const currentColor = getPixel(pixelData, x, y);
+
+                if (currentColor === targetColor) {
+                    pixelData.data[y * pixelData.width + x] = fillColor;
+                    pixelsToCheck.push(x + 1, y);
+                    pixelsToCheck.push(x - 1, y);
+                    pixelsToCheck.push(x, y + 1);
+                    pixelsToCheck.push(x, y - 1);
+                }
+            }
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+
+    const handleMouseDown = (e: MouseEvent<HTMLCanvasElement>) => {
+        if (!canDraw) {
+            return;
+        }
+
+        const {offsetX, offsetY} = e.nativeEvent;
+
+        switch(toolType) {
+            case ToolType.Brush:
+                contextRef.current?.beginPath();
+                contextRef.current?.moveTo(offsetX, offsetY);
+                contextRef.current?.lineTo(offsetX, offsetY);
+                contextRef.current?.stroke();
+
+                const cords = {
+                    start: {
+                        x: offsetX / width,
+                        y: offsetY / height
+                    },
+                    end: {
+                        x: offsetX / width,
+                        y: offsetY / height
+                    }
+                };
+                if (onDraw)
+                    onDraw(cords);
+
+                setPrevCords(cords.end);
+                setIsDrawing(true);
+                break;
+            case ToolType.Fill:
+                console.log("Filling");
+                fill(contextRef.current!, {
+                    x: offsetX,
+                    y: offsetY
+                });
+                console.log("Done");
+                break;
+        }
     }
 
     const handleMouseUp = () => {
@@ -50,7 +157,30 @@ export default function Canvas() {
     const handleMouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
         if (!isDrawing) { return; }
         const {offsetX, offsetY} = e.nativeEvent;
+
         contextRef.current?.lineTo(offsetX, offsetY);
+        contextRef.current?.stroke();
+
+
+        const cords = {
+            start: prevCords!,
+            end: {
+                x: offsetX / width,
+                y: offsetY / height
+            }
+        };
+
+        if (onDraw && prevCords) {
+            onDraw(cords);
+        }
+
+        setPrevCords(cords.end);
+    }
+
+    const draw = (cordsDraw: CordsSet) => {
+        contextRef.current?.beginPath();
+        contextRef.current?.moveTo(cordsDraw.start.x * width, cordsDraw.start.y * height);
+        contextRef.current?.lineTo(cordsDraw.end.x * width, cordsDraw.end.y * height);
         contextRef.current?.stroke();
     }
 
@@ -61,7 +191,7 @@ export default function Canvas() {
         onMouseOut={() => { setIsDrawing(false) }}
         style={{backgroundColor: "white"}}
         ref={canvasRef}
-        width={'1500'}
-        height={'900'}
+        width={width}
+        height={height}
     />
 }
--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -1,8 +1,61 @@
 import ChatBubble from "@/components/atoms/Chat-Bubble/Chat-Bubble";
-import Canvas from '@/components/atoms/Canvas/Canvas';
+import Canvas, {CordsSet, ToolType} from '@/components/atoms/Canvas/Canvas';
+import {ChangeEvent, useEffect, useState} from 'react';
 
 export default function GamePage () {
-    return (
-        <Canvas></Canvas>
-    )
+    const [cordsDraw, setCordsDraw] = useState<CordsSet | null>(null);
+    const [canDraw, setCanDraw] = useState(false);
+    const [color, setColor] = useState("#000000");
+    const [lineWidth, setLineWidth] = useState(1);
+    const [toolType, setToolType] = useState<ToolType>(ToolType.Brush);
+
+    return (<>
+        <Canvas
+            width={1500}
+            height={900}
+            lineWidth={lineWidth}
+            color={color}
+            canDraw={canDraw}
+            cordsDraw={cordsDraw}
+            toolType={toolType}
+        />
+
+        <button onClick={() => setInterval(() => {
+            setCordsDraw({
+                start: {
+                    x: Math.random(),
+                    y: Math.random()
+                },
+                end: {
+                    x: Math.random(),
+                    y: Math.random()
+                }
+            })
+        }, 1000)}>
+            Draw test
+        </button>
+
+        <button onClick={() => setCanDraw(!canDraw)}>
+            Set Draw
+        </button>
+
+        <input onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            // @ts-ignore
+            setColor(event.nativeEvent.target!.value);
+        }} type={'color'} />
+        <input min={1} max={10} type={'range'} onChange={(event) => {
+            //@ts-ignore
+            setLineWidth((event.nativeEvent.target!.value));
+        }} />
+
+        <select onChange={(e) => {
+            //@ts-ignore;
+            console.log(e.nativeEvent.target.value);
+            //@ts-ignore
+            setToolType(Number(e.nativeEvent.target.value));
+        }}>
+            <option value={ToolType.Brush}>Brush</option>
+            <option value={ToolType.Fill}>Fill</option>
+        </select>
+    </>)
 }
