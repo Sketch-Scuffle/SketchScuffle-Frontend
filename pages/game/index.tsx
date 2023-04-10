e575b1e70e2cbed10ab1590c5fd60e77057a5898
Pociatlo <k.podsadlo@gmail.com>
2023-04-10 23:59:30 +0200

    button simple content done

--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -2,6 +2,6 @@ import Button from "@/components/atoms/Button/Button";
 import SendSVG from '@/public/icons/send.svg'
 export default function GamePage () {
     return (
-        <Button size={"medium"} variant={"signin"}><p>Join</p></Button>
+        <Button size={"small"} variant={1}> <SendSVG /> </Button>
     )
 }
