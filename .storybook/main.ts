6a771bbd84f399aaf8381520eddecaa9118488f0
Pociatlo <k.podsadlo@gmail.com>
2023-05-07 20:48:37 +0200

    external assets folder for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 import * as path from 'path';
 
 const config: StorybookConfig = {
-  staticDirs: ['../public'],
+  staticDirs: ['../public','../.storybook/assets'],
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
