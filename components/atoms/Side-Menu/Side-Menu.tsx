935690eb77bdb16ef5b626253d21f609f6d84a10
Pociatlo <k.podsadlo@gmail.com>
2023-05-08 20:43:18 +0200

    side menu done

---
+++ components/atoms/Side-Menu/Side-Menu.tsx
@@ -0,0 +1,36 @@
+import React, { PropsWithChildren, useState } from "react";
+import styles from "@/components/atoms/Side-Menu/Side-Menu.module.scss";
+import cs from "classnames";
+
+export interface SideMenuProps {
+    tabs: {
+        [key: string]: React.ReactNode;
+    };
+}
+
+export default function SideMenu({ tabs }: PropsWithChildren<SideMenuProps>) {
+    const [activeTab, setActiveTab] = useState(Object.keys(tabs)[0]);
+    console.log(activeTab)
+    return (
+        <div className={styles.sideMenuWrapper}>
+            <div className={styles.cardWrapper}>
+                <div className={cs(styles.markSquare,{
+                    [styles.chat]: activeTab === "Chat",
+                    [styles.scoreboard]: activeTab === "Scoreboard",
+                })}></div>
+                    {Object.keys(tabs).map((tab) => (
+                        <>
+
+                        <div
+                            key={tab}
+                            onClick={() => setActiveTab(tab)}
+                        >
+                            {tab}
+                        </div>
+                        </>
+                    ))}
+            </div>
+            <div className={styles.tabContent}>{tabs[activeTab]}</div>
+        </div>
+    );
+}
